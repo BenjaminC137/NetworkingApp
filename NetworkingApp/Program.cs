f851f7094dbd70f2a34de7ce8c23518d63296200
Benjamin <benjaminsalasek@Athena.local>
2018-07-23 10:14:53 -0500

    initial

---
+++ NetworkingApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NetworkingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Net.Sockets.UdpClient udp = new System.Net.Sockets.UdpClient(15000);
+            System.Net.IPEndPoint iP = new System.Net.IPEndPoint(System.Net.IPAddress.Any, 15000);
+        }
+    }
+}
