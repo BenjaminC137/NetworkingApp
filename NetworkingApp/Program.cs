0a2873b6a36e11d112384ff3383459affc7a7f28
Benjamin <benjaminsalasek@Athena.local>
2018-07-23 11:38:31 -0500

    more from class

--- NetworkingApp/Program.cs
+++ NetworkingApp/Program.cs
@@ -1,13 +1,41 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Text;
 
 namespace NetworkingApp
 {
     class Program
     {
+        private static System.Net.Sockets.UdpClient udp = new System.Net.Sockets.UdpClient(15000);
+        private static System.Net.IPEndPoint ip = new System.Net.IPEndPoint(System.Net.IPAddress.Broadcast, 15000);
+
+        private static void Listen()
+        {
+            udp.BeginReceive((iar) =>
+            {
+                byte[] bytes = udp.EndReceive(iar, ref ip);
+                string message = System.Text.Encoding.ASCII.GetString(bytes);
+                Console.WriteLine(message);
+                Listen();
+            }, new object());
+        }
         static void Main(string[] args)
         {
-            System.Net.Sockets.UdpClient udp = new System.Net.Sockets.UdpClient(15000);
-            System.Net.IPEndPoint iP = new System.Net.IPEndPoint(System.Net.IPAddress.Any, 15000);
+            Console.WriteLine("Enter your name");
+            string yourName = Console.ReadLine();
+            string typedMessage = "";
+
+            Listen();
+            do
+            {
+                typedMessage = Console.ReadLine();
+                byte[] encodedMessage = System.Text.Encoding.ASCII.GetBytes(yourName + ": " + typedMessage);
+                string ipAddress = "192.168.1.40";
+                var recipient = new System.Net.IPEndPoint(System.Net.IPAddress.Parse(ipAddress), 15000);
+                udp.Send(encodedMessage, encodedMessage.Length, recipient);
+            }
+                while (typedMessage != "exit");
+
         }
     }
 }
